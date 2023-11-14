f9c2882915fda8dba334a9ea00cca1fdf5099936
Pineapple <danieltrabajo1618@gmail.com>
2023-11-13 23:14:27 -0300

    Solución a problemas de menú del teléfono

--- js/nav.js
+++ js/nav.js
@@ -4,9 +4,5 @@ function openNav(){
 
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
-    document.addEventListener('click', (e) => {
-        openNav()
-        e.preventDefault
-    })
 }
 
