85e81fd9444c38d50b18fdbae7f6f6454dcd8244
Pineapple <danieltrabajo1618@gmail.com>
2023-11-13 22:11:12 -0300

    prueba de fallo con enlances

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,5 @@ function openNav(){
 
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
-    document.addEventListener('click', (e) => {
-        e.preventDefault()
-    });
+        document.preventDefault(closeNav)
 }
